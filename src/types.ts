061e572da3a2b651d668b948870804042cea12ff
Jeton <tonni-k@hotmail.com>
2024-08-08 22:02:44 +0200

    chore: include product interface

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,10 @@ export interface Customer {
     since: string;
     revenue: string;
 }
+
+export interface Product {
+    id: string;
+    description: string;
+    category: string;
+    price: string;
+}
