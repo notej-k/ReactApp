40f3b508ad6b78468657b33807cd660b399008dd
Jeton <tonni-k@hotmail.com>
2024-08-08 22:03:01 +0200

    also add api helper for fetching product/products

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 // mock fetching data from server.
-import { Customer } from "../types";
+import { Customer, Product } from "../types";
 import { Order } from "../components/orders/types";
 import customers from "../data/customers";
 import orders from "../data/orders";
+import products from "../data/products";
 
-const fetchOrderById = (orderId: string): Promise<Order> => {
+export const fetchOrderById = (orderId: string): Promise<Order> => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const order = orders.find((order) => order.id === orderId);
@@ -33,6 +34,27 @@ export const fetchCustomerById = (customerId: string): Promise<Customer> => {
         resolve(customer);
       } else {
         reject(new Error("Customer not found"));
+      }
+    }, 2000);
+  });
+};
+
+export const fetchAllProducts = (): Promise<Product[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 2000);
+  });
+};
+
+export const fetchProductById = (productId: string): Promise<Product> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const product = products.find((product) => product.id === productId);
+      if (product) {
+        resolve(product);
+      } else {
+        reject(new Error("Product not found"));
       }
     }, 2000);
   });
