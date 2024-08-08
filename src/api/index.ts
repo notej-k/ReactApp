d594236e1e3c96bd6f05f45f5180e459beecc76f
Jeton <tonni-k@hotmail.com>
2024-08-08 21:21:05 +0200

    chore: add api helper for fetching customer by id

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 // mock fetching data from server.
+import { Customer } from "../types";
 import { Order } from "../components/orders/types";
+import customers from "../data/customers";
 import orders from "../data/orders";
 
 const fetchOrderById = (orderId: string): Promise<Order> => {
@@ -19,6 +21,19 @@ export const fetchAllOrders = (): Promise<Order[]> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(orders);
+    }, 2000);
+  });
+};
+
+export const fetchCustomerById = (customerId: string): Promise<Customer> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const customer = customers.find((customer) => customer.id === customerId);
+      if (customer) {
+        resolve(customer);
+      } else {
+        reject(new Error("Customer not found"));
+      }
     }, 2000);
   });
 };
