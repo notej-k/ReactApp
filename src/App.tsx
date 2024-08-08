94fc58531396819a7dcecbaf8618a73a57849c5f
Jeton <tonni-k@hotmail.com>
2024-08-08 13:08:21 +0200

    feat: introduce Layout component as a wrapper for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
 import './index.css';
+import Layout from "./common/Layout";
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
-};
+    return (
+      <BrowserRouter>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </BrowserRouter>
+    );
+  };
 
 export default App;
---
+++ src/common/Layout/index.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import Navbar from '../../components/common/Navbar';
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <main className="container mx-auto py-4">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
