7d3da340e7225f8efd447a52947c26bb4bf5768b
Jeton <tonni-k@hotmail.com>
2024-08-08 23:53:48 +0200

    chore: initialize reducer orders from root

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
-import './index.css';
+import "./index.css";
 import Layout from "./components/common/Layout";
+import { initialOrderState, orderReducer } from "./components/orders/reducer";
+import orders from "./data/orders";
+import { initializeOrders } from "./components/orders/actions";
+import { storeOrdersInLocalStorage } from "./helpers/LocalStorage";
 
 const App: React.FC = () => {
-    return (
-      <BrowserRouter>
-        <Layout>
-          <AppRoutes />
-        </Layout>
-      </BrowserRouter>
-    );
-  };
+  const [, dispatch] = useReducer(orderReducer, initialOrderState);
+
+  useEffect(() => {
+    const ordersInLocalStorage = localStorage.getItem("orders");
+    if (!ordersInLocalStorage) {
+      storeOrdersInLocalStorage(orders);
+    }
+
+    dispatch(initializeOrders(orders));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
