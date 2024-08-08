93b6a0e6348e1afc44d5396bfea7daa18751aace
Jeton <tonni-k@hotmail.com>
2024-08-09 00:39:55 +0200

    redirect to orders as its the only page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import OrderList from "./components/orders/List";
 import OrderDetail from "./components/orders/Detail";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Home page</div>} />
+      <Route path="/" element={<Navigate to="/orders" />} />
       <Route path="/orders" element={<OrderList />} />
       <Route path="/orders/:id" element={<OrderDetail />} />
       <Route path="*" element={<div>Not found!</div>} />
