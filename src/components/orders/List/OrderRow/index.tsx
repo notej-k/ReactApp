a404cac62813fd6dc788b7dc7face787f19efb6d
Jeton <tonni-k@hotmail.com>
2024-08-08 21:24:49 +0200

    chore: click on order row and go to detail

--- src/components/orders/List/OrderRow/index.tsx
+++ src/components/orders/List/OrderRow/index.tsx
@@ -3,12 +3,14 @@ import { Order } from "../../types";
 import { fetchCustomerById } from "../../../../api";
 import { Customer } from "../../../../types";
 import LoadingIndicator from "../../../common/LoadingIndicator";
+import { useNavigate, useNavigation } from "react-router";
 
 interface OrderRowProps {
   order: Order;
 }
 
 const OrderRow: React.FC<OrderRowProps> = ({ order }) => {
+  const navigate = useNavigate();
   const [customer, setCustomer] = useState<Customer | null>(null);
   const [isCustomerLoading, setIsCustomerLoading] = useState(false);
 
@@ -28,7 +30,11 @@ const OrderRow: React.FC<OrderRowProps> = ({ order }) => {
   }, []);
 
   return (
-    <tr key={`${order.id}`}>
+    <tr
+      className="cursor-pointer"
+      onClick={() => navigate(`/orders/${order.id}`)}
+      key={`${order.id}`}
+    >
       <td className="py-2 px-4 border-b">{order.id}</td>
       <td className="py-2 px-4 border-b">{customer?.name ?? "..."}</td>
       <td className="py-2 px-4 border-b">{quantity}</td>
