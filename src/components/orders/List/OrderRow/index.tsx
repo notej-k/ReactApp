090f9c57e8e65ff11bba5f643e92abe045fd25fb
Jeton <tonni-k@hotmail.com>
2024-08-08 21:22:11 +0200

    chore: add order row component

---
+++ src/components/orders/List/OrderRow/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Order } from "../../types";
+import { fetchCustomerById } from "../../../../api";
+import { Customer } from "../../../../types";
+import LoadingIndicator from "../../../common/LoadingIndicator";
+
+interface OrderRowProps {
+  order: Order;
+}
+
+const OrderRow: React.FC<OrderRowProps> = ({ order }) => {
+  const [customer, setCustomer] = useState<Customer | null>(null);
+  const [isCustomerLoading, setIsCustomerLoading] = useState(false);
+
+  const quantity = order.items.reduce((sum, item) => {
+    return sum + parseInt(item.quantity);
+  }, 0);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsCustomerLoading(true);
+      const data = await fetchCustomerById(order["customer-id"]);
+      setCustomer(data);
+      setIsCustomerLoading(false);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <tr key={`${order.id}`}>
+      <td className="py-2 px-4 border-b">{order.id}</td>
+      <td className="py-2 px-4 border-b">{customer?.name ?? "..."}</td>
+      <td className="py-2 px-4 border-b">{quantity}</td>
+      <td className="py-2 px-4 border-b">${order.total}</td>
+    </tr>
+  );
+};
+
+export default OrderRow;
