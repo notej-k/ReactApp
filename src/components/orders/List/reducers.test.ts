56c684b05beea42df8392f7e328ebeeb1eb7a24d
Jeton <tonni-k@hotmail.com>
2024-08-09 16:52:45 +0200

    tests: adding a new item to an existing order.

--- src/components/orders/List/reducers.test.ts
+++ src/components/orders/List/reducers.test.ts
@@ -1,6 +1,7 @@
-import { INITIALIZE_ORDERS, SetOrdersAction } from '../actions';
+import { storeOrdersInLocalStorage } from '../../../helpers/LocalStorage';
+import { ADD_ITEM, AddItemAction, INITIALIZE_ORDERS, SetOrdersAction } from '../actions';
 import { initialOrderState, orderReducer } from '../reducer';
-import { Order } from '../types';
+import { Order, OrderItem } from '../types';
 
 jest.mock('../../../helpers/LocalStorage');
 
@@ -26,5 +27,33 @@ describe('orderReducer', () => {
         expect(newState.orders).toEqual(initialOrders);
     });
 
+    it('should add a new item to an existing order', () => {
+        const action: AddItemAction = {
+            type: ADD_ITEM,
+            payload: {
+                orderId: '1',
+                productId: 'prod-1',
+                quantity: 2,
+                productPrice: '10.00',
+            },
+        };
 
+        const state = {
+            orders: initialOrders,
+        };
+
+        const expectedItems: OrderItem[] = [
+            {
+                'product-id': 'prod-1',
+                quantity: '2',
+                'unit-price': '10.00',
+                total: '20.00',
+            },
+        ];
+
+        const newState = orderReducer(state, action);
+        expect(newState.orders[0].items).toEqual(expectedItems);
+        expect(newState.orders[0].total).toBe('20.00');
+        expect(storeOrdersInLocalStorage).toHaveBeenCalledWith(newState.orders);
+    });
 });
